53648a2bd6d723fa053dd14ae9c6cce077a1bdd8
Faliirham <falimagemaulana64@gmail.com>
2023-10-03 12:19:18 +0700

    commit

--- jobsheet6/Pemilihan2Percobaan310.java
+++ jobsheet6/Pemilihan2Percobaan310.java
@@ -13,7 +13,7 @@ public class Pemilihan2Percobaan310 {
         System.out.print(" Masukkan Besarnya penghasilan : ");
         penghasilan = input10.nextInt();
 
-        if(kategori.equals("pekerja")){
+        if(kategori.equalsIgnoreCase("pekerja")){
             if(penghasilan <= 2000000){
                 pajak = 0.1;
             }else if (penghasilan <= 3000000){
@@ -23,7 +23,7 @@ public class Pemilihan2Percobaan310 {
             }   
             gajiBersih =(int) (penghasilan -(pajak * penghasilan));
             System.out.print("Penghasilan  Bersih : " +gajiBersih); 
-        }else if (kategori.equals("pebisnis")){
+        }else if (kategori.equalsIgnoreCase("pebisnis")){
             if(penghasilan <= 2500000){
                 pajak = 0.15;
             }else if (penghasilan <=3500000){
