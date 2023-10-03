bbb6fce6a031619a1cb4200b0376f38ef405d04c
Faliirham <falimagemaulana64@gmail.com>
2023-10-03 07:51:54 +0700

    commit

---
+++ jobsheet6/Pemilihan2Percobaan210.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan210 {
+    public static void main(String[] args) {
+        Scanner input10 = new Scanner(System.in);
+
+        
+    }
+}
