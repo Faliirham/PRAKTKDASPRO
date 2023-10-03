46e06b893907b5c9afa78914a9a96d7783dd805b
Faliirham <falimagemaulana64@gmail.com>
2023-10-03 10:20:07 +0700

    commit

--- jobsheet6/Pemilihan2Percobaan210.java
+++ jobsheet6/Pemilihan2Percobaan210.java
@@ -20,7 +20,7 @@ public class Pemilihan2Percobaan210 {
             System.out.println(" Segitiga tersebut adalah segitiga siku siku");
             }else if ((sudut1 == sudut2) || (sudut1==sudut3) || (sudut2 == sudut3)){
             System.out.println("Segitiga tersebut adalah segitiga sama kaki ");
-            }else if ((sudut1==sudut2) && (sudut2==sudut3)){
+            }else if ((sudut1==60) && (sudut2==60) && (sudut3==60)){
             System.out.println("Segitiga tersebut adalah segitiga sama sisi ");
             }else if ((sudut1<=90)&&(sudut2<=90)&&(sudut3<=90)){
                 System.out.println("Segitiga tersebut adalah segitiga lancip");
