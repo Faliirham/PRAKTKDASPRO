345b9f20032525ef471a06399efbf253b0bf30d7
Faliirham <falimagemaulana64@gmail.com>
2023-11-01 08:02:43 +0700

    commit

--- jobsheet10/BioskopWithScanner10.java
+++ jobsheet10/BioskopWithScanner10.java
@@ -1,7 +1,7 @@
 package jobsheet10;
 import java.util.Scanner;
 
-public class BisokopwithScanner10 {
+public class BioskopWithScanner10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
